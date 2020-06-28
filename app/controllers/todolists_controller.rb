6aabe1f7980be022146654756878583fd325bb3e
vagrant <vagrant@localhost.localdomain>
2020-06-28 06:05:01 +0000

    [Finish] 3-5

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,4 +1,21 @@
 class TodolistsController < ApplicationController
-  def new
-  end
+    def new
+  	  #viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
+  	  @list = List.new
+    end
+
+    def create
+  	 # ストロングパラメータを使用
+  	  list = List.new(list_params)
+  	 # DBへ保存する
+  	  list.save
+  	 # トップ画面へリダイレクト
+  	  redierect_to '/top'
+  	end
+    private
+
+  	def list_params
+　　　　　params.require(:list).permit(:title, :body)
+  	end
+
 end
