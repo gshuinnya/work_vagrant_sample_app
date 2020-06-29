f07a0b05645c1b4bc80a7b5e467a4cb134ee02e3
vagrant <vagrant@localhost.localdomain>
2020-06-29 04:32:55 +0000

    [Finish] 3-7

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -3,7 +3,6 @@ class TodolistsController < ApplicationController
   	  #viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
   	  @list = List.new
     end
-
     def create
   	 # ストロングパラメータを使用
   	  list = List.new(list_params)
@@ -12,16 +11,22 @@ class TodolistsController < ApplicationController
   	 # トップ画面へリダイレクト
   	  redirect_to todolist_path(list.id)
   	end
-    
     def index
     	@lists = List.all
   	end
-
   	def show
   		@list = List.find(params[:id])
   	end
-    private
+  	def edit
+  		@list = List.find(params[:id])
+  	end
+  	def update
+  		list = List.find(params[:id])
+  		list.update(list_params)
+  		redirect_to todolist_path(list.id)
+  	end
 
+    private
   	def list_params
         params.require(:list).permit(:title, :body)
   	end
